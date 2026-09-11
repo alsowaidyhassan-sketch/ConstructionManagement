05c199543613ca608ad6149bf20966673df06d3a
alsowaidyhassan-sketch <alsowaidyhassan@gmail.com>
2026-09-11 10:16:55 +0300

    feat: redirect root path to swagger UI

--- src/Api/ConstructionManagement.Api/Program.cs
+++ src/Api/ConstructionManagement.Api/Program.cs
@@ -87,6 +87,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
+app.MapGet("/", () => Results.Redirect("/swagger"));
+
 app.MapControllers();
 
 app.Run();
