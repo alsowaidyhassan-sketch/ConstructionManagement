4b08f4ab203d5ca078710e8d9cdc1667356ec09f
alsowaidyhassan-sketch <alsowaidyhassan@gmail.com>
2026-09-11 10:25:01 +0300

    refactor: update project startup configuration
    Move startup logic to Sub Main and remove unnecessary project property overrides to streamline the application entry point.

--- src/Desktop/ConstructionManagement.WinForms/Program.vb
+++ src/Desktop/ConstructionManagement.WinForms/Program.vb
@@ -1,13 +1,10 @@
 Imports System.Windows.Forms
-Imports ConstructionManagement.WinForms.Forms
 
-Namespace ConstructionManagement.WinForms
-    Public Module ProgramModule
-        <STAThread>
-        Public Sub Main()
-            Application.EnableVisualStyles()
-            Application.SetCompatibleTextRenderingDefault(False)
-            Application.Run(New LoginForm())
-        End Sub
-    End Module
-End Namespace
+Public Module ProgramModule
+    <STAThread>
+    Public Sub Main()
+        Application.EnableVisualStyles()
+        Application.SetCompatibleTextRenderingDefault(False)
+        Application.Run(New LoginForm())
+    End Sub
+End Module
